0540a3ec96eef2bb345c0f1a676289c55df2b3fc
James Burnside <jcburnside97@gmail.com>
2019-12-01 11:49:55 -0500

    Day 1 part 1 solution

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Day1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting fuel calculations");
+            var masses = (await File.ReadAllLinesAsync("DayOneInput.txt")).Select(s=>int.TryParse(s,out int i) ? i : 0);
+            int total_fuel = 0;
+            foreach(int mass in masses)
+            {
+                total_fuel += (int)Math.Floor(mass / 3.0) - 2;
+            }
+            Console.WriteLine($"Total Fuel Needed:{total_fuel}");
         }
     }
 }
