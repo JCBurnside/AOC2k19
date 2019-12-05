91c04b52076b05266f24a665c907ec0bf2519a8c
James Burnside <jcburnside97@gmail.com>
2019-12-04 21:25:12 -0500

    day 1 part 2 done

--- day1/Program.cs
+++ day1/Program.cs
@@ -10,13 +10,19 @@ namespace Day1
         public static async Task Main(string[] args)
         {
             Console.WriteLine("Starting fuel calculations");
-            var masses = (await File.ReadAllLinesAsync("DayOneInput.txt")).Select(s=>int.TryParse(s,out int i) ? i : 0);
-            int total_fuel = 0;
-            foreach(int mass in masses)
+            var masses = (await File.ReadAllLinesAsync("DayOneInput.txt")).Select(s=>int.TryParse(s,out int i) ? i : 0).ToList();
+            long total_fuel = 0;
+            for(int ctr=0; ctr<masses.Count(); ctr++)
             {
-                total_fuel += (int)Math.Floor(mass / 3.0) - 2;
+                int change_in_fuel = (int)Math.Floor(masses[ctr] / 3.0) - 2;
+                if(change_in_fuel>0)
+                {
+                    masses.Add(change_in_fuel);
+                    total_fuel += change_in_fuel;
+                }
             }
-            Console.WriteLine($"Total Fuel Needed:{total_fuel}");
+            Console.WriteLine($"Total Fuel Needed :{total_fuel}");
+
         }
     }
 }
